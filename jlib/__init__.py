261f1a6c9a2a6d596b6738fc740db5dc1b35741f
Jayson Larose <jayson@interlaced.org>
2021-03-09 03:13:55 -0800

    * Added "scanwalk" function, which is just `os.walk`, but tweaked so it   returns os.DirEntry objects instead of lameo paths.

--- jlib/__init__.py
+++ jlib/__init__.py
@@ -10,7 +10,7 @@ else:
 import os, atexit, collections, argparse, enum, string
 from threading import Thread
 
-__version__ = "1.0.10"
+__version__ = "1.0.11"
 
 image_exts = set('.bmp .cur .dcx .eps .fli .fpx .gbr .gif .icns .ico .im .imt .iptc .jpe .jpeg .jpg .jp2 .mpo .msp .pbm .pcd .pcx .png .ppm .psd .svg .tga .tif .tiff .wal .xbm .xpm .vtx .webp'.split())
 video_exts = set('.wmv .mpeg .mpg .asf .rm .rmvb .ram .flv .mov .mkv .m4v .webm .3g .3gpp .3gp .mp4 .avi .divx .vob'.split())
@@ -1324,6 +1324,99 @@ class FakeDirEntry:
 			import stat
 			self._is_symlink = stat.S_ISLNK(self.stat(follow_symlinks=False).st_mode)
 		return self._is_symlink
+# }}}
+
+def scanwalk(top, topdown=True, onerror=None, followlinks=False):# {{{
+    """
+	Just like os.walk, except it returns juicy fresh os.DirEntry objects,
+	rather than overcooked, sad, lumpy paths.
+
+	In fact, this is almost EXACTLY like os.walk, because I copied
+	walk straight out of python 3.8's os module, and modified a couple
+	of bits. I'm definitely not laying claim to this code by including
+	it in my library.
+    """
+    top = os.fspath(top)
+    dirs = []
+    nondirs = []
+    walk_dirs = []
+
+    # We may not have read permission for top, in which case we can't
+    # get a list of the files the directory contains.  os.walk
+    # always suppressed the exception then, rather than blow up for a
+    # minor reason when (say) a thousand readable directories are still
+    # left to visit.  That logic is copied here.
+    try:
+        # Note that scandir is global in this module due
+        # to earlier import-*.
+        scandir_it = os.scandir(top)
+    except OSError as error:
+        if onerror is not None:
+            onerror(error)
+        return
+
+    with scandir_it:
+        while True:
+            try:
+                try:
+                    entry = next(scandir_it)
+                except StopIteration:
+                    break
+            except OSError as error:
+                if onerror is not None:
+                    onerror(error)
+                return
+
+            try:
+                is_dir = entry.is_dir()
+            except OSError:
+                # If is_dir() raises an OSError, consider that the entry is not
+                # a directory, same behaviour than os.path.isdir().
+                is_dir = False
+
+            if is_dir:
+                dirs.append(entry)
+            else:
+                nondirs.append(entry)
+
+            if not topdown and is_dir:
+                # Bottom-up: recurse into sub-directory, but exclude symlinks to
+                # directories if followlinks is False
+                if followlinks:
+                    walk_into = True
+                else:
+                    try:
+                        is_symlink = entry.is_symlink()
+                    except OSError:
+                        # If is_symlink() raises an OSError, consider that the
+                        # entry is not a symbolic link, same behaviour than
+                        # os.path.islink().
+                        is_symlink = False
+                    walk_into = not is_symlink
+
+                if walk_into:
+                    walk_dirs.append(entry.path)
+
+    # Yield before recursion if going top down
+    if topdown:
+        yield top, dirs, nondirs
+
+        # Recurse into sub-directories
+        islink, join = os.path.islink, os.path.join
+        for dirname in dirs:
+            new_path = join(top, dirname)
+            # Issue #23605: os.path.islink() is used instead of caching
+            # entry.is_symlink() result during the loop on os.scandir() because
+            # the caller can replace the directory entry during the "yield"
+            # above.
+            if followlinks or not islink(new_path):
+                yield from scanwalk(new_path, topdown, onerror, followlinks)
+    else:
+        # Recurse into sub-directories
+        for new_path in walk_dirs:
+            yield from scanwalk(new_path, topdown, onerror, followlinks)
+        # Yield after recursion if going bottom up
+        yield top, dirs, nondirs
 # }}}
 
 # HACKS
